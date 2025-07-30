d9ad53e735a4d45bf8318ecb26242dfc7de6e0da
ikkikana <hei.princy@gmail.com>
2025-07-30 16:09:15 +0300

    Gauche final image soloina sisa

--- src/components/PokedexPage.jsx
+++ src/components/PokedexPage.jsx
@@ -4,7 +4,7 @@ import PokemonListSidebar from "./PokemonListSidebar.jsx";
 export default function PokedexPage() {
     return(
         <div className="min-h-screen overflow-hidden">
-            <div className="absolute inset-0 z-0 bg-cover bg-center brightness-75" style={{backgroundImage: "url('/Image/pixelart.gif')"}} />
+            <div className="absolute inset-0 z-0 bg-cover bg-center brightness-75" style={{backgroundImage: "url('/Image/pixelart sunset.gif')"}} />
             <div className="relative z-10  flex gap-10 pt-10 pr-5 pb-10">
                 <div className="w-[70%]">
                     <PokemonDetailCard/>
